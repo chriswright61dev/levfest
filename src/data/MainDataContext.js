77ac821f9145fd2f7eba3efad03e1d1487586c1e
Chris Wright <chriswrightdev@gmail.com>
2021-05-26 17:25:11 +0100

    change to use reducer

--- src/data/MainDataContext.js
+++ src/data/MainDataContext.js
@@ -1,12 +1,37 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 
 export const MainDataContext = createContext();
 function MainDataContextProvider(props) {
-  const [mainState, setMainState] = useState({
-    news: "news",
-    events: "events",
-    venues: "venues",
-  });
+  const initialState = {
+    venuesListLoaded: false,
+    eventsListLoaded: false,
+    newsListLoaded: false,
+    feedsCount: 0,
+    loadCount: 0,
+    venuesListData: [],
+    venuesLongData: [],
+    eventsListData: [],
+    eventsListDataOld: [],
+    eventsLongData: [],
+    newsListData: [],
+    newsLongData: [],
+  };
+
+  const [mainState, dispatch] = useReducer(reducerfn, initialState);
+
+  function reducerfn(mainState, action) {}
+
+  const thisyear = new Date().getFullYear();
+  // get data from these urls
+  const mainApiListsURLs = {
+    // api urls object
+    venuesList: "http://www.levenshulmecommunityfestival.co.uk/api_venues_list",
+    eventsList:
+      "http://www.levenshulmecommunityfestival.co.uk/api_levfest_events_list/" +
+      thisyear,
+
+    newsList: "http://www.levenshulmecommunityfestival.co.uk/api_news_list",
+  };
 
   return (
     <MainDataContext.Provider value={mainState}>
