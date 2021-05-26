d8ff1fa21786bc5059007576f617b4b4beba5775
Chris Wright <chriswrightdev@gmail.com>
2021-05-26 15:17:23 +0100

    add links to navigation

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
 function Navigation() {
-  return <div>navigation</div>;
+  return (
+    <nav className="header-links">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+        <li>
+          <Link to="/events">Events</Link>
+        </li>
+        <li>
+          <Link to="/venues">Venues</Link>
+        </li>
+        <li>
+          <Link to="/events_old">Old Events</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
